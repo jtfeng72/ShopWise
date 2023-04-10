4a56d208bc2921afd16688c103f739a5f7be5ba0
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-10 14:10:45 -0500

    Update shopWise_app.py

--- shopWise_app.py
+++ shopWise_app.py
@@ -1,1 +1,24 @@
 # ShopWise.py
+# Import libraries
+import streamlit as st
+import pandas as pd
+
+# Page setup
+st.set_page_config(page_title='ShopWise', page_icon=':bar_chart:', layout='wide')
+st.title('Welcome to ShopWise')
+
+# Connect to the Google Sheet
+sheet_id = "1X5ANn3c5UKfpc-P20sMRLJhHggeSaclVfXavdfv-X1c"
+sheet_name = "Food_List_Master"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+df = pd.read_csv(url, dtype=str).fillna("")
+
+# Build a user interface and search functionality
+text_search = st.text_input("Search items by item description", value="")
+
+m1 = df["Name"].str.contains(text_search)
+m2 = df["Catagory"].str.contains(text_search)
+df_search = df[m1 | m2]
+
+if text_search:
+    st.write(df_search)
