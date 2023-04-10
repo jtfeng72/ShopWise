0f06454ebada7124c89d530250bb72461dcbe061
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-10 16:31:04 -0500

    Update shopWise_app.py

--- shopWise_app.py
+++ shopWise_app.py
@@ -8,16 +8,10 @@ st.set_page_config(page_title='ShopWise', page_icon=':bar_chart:', layout='wide'
 st.title('Welcome to ShopWise')
 
 # Connect to the Google Sheet
-
-@set.cache
-def get_dat_from_sheet():
-    sheet_id = "1X5ANn3c5UKfpc-P20sMRLJhHggeSaclVfXavdfv-X1c"
-    sheet_name = "Food_List_Master"
-    url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
-    df = pd.read_csv(url, dtype=str).fillna("")
-    return df
-
-df = ger_data_from_sheet()
+sheet_id = "1X5ANn3c5UKfpc-P20sMRLJhHggeSaclVfXavdfv-X1c"
+sheet_name = "Food_List_Master"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+df = pd.read_csv(url, dtype=str).fillna("")
  
 # Build a user interface and search functionality
 text_search = st.text_input("Search items by item description", value="")
