c68e5d5b32433abc2c2db05691057fc1772d1859
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-12 16:22:18 -0500

    Update test2_shopWise_app.py

--- test2_shopWise_app.py
+++ test2_shopWise_app.py
@@ -35,5 +35,5 @@ content = read_file(bucket_name, file_path)
 # Print results.
 for line in content.strip().split("\n"):
     Product_ID, Name, CO2eq_per_Kg, Catagory, Days_in_Pantry, Days_in_Fridge, Days_in_Freezer = line.split(",")
-    st.write(f"{name} has a :{pet}:")
+    st.write(f"{Product_ID} has a :{Name}:")
 
