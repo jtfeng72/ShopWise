af0f027d1f16f14dc94fa88fef8f016cba6190d3
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-14 11:45:29 -0500

    Update shopWise_app.py

--- shopWise_app.py
+++ shopWise_app.py
@@ -1,9 +1,16 @@
 # ShopWise.py
 #---Import libraries---#
-import streamlit as st
+from collections import namedtuple
+from streamlit_option_menu import option_menu
+import altair as alt
+import math
 import pandas as pd
+import streamlit as st
+from deta import Deta
+from PIL import Image
 from st_aggrid import AgGrid, JsCode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
+
 
 #---Page setup---#
 st.set_page_config(page_title='ShopWise', page_icon=':bar_chart:', layout='wide')
