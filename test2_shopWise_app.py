acdacc468c7446e4144596c5a560a01f50172b8e
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-14 15:53:32 -0500

    Update test2_shopWise_app.py

--- test2_shopWise_app.py
+++ test2_shopWise_app.py
@@ -25,7 +25,7 @@ credentials = service_account.Credentials.from_service_account_info(
 )
 client = storage.Client(credentials=credentials)
 
-#  ----- Retrieve file contents  ----- #
+#  ----- Retrieve file contents into a DataFrame ----- #
 
 bucket_name = "shopwise-bucket"
 file_path = "Food_List.csv"
@@ -45,9 +45,106 @@ def read_csv_from_gcs(bucket_name, file_path):
   dataframe = pd.read_csv(StringIO(csv_data))
   return dataframe
 
-# ----- Read the CSV file and load it into a DataFrame ----- #
 df = read_csv_from_gcs(bucket_name, file_path)
 
-AgGrid(df)
+#  ----- Build a user interface and search functionality ----- #
+text_search = st.text_input("Search items by item description", value="")
 
+m1 = df["Name"].str.contains(text_search)
+m2 = df["Catagory"].str.contains(text_search)
+df_search = df[m1 | m2]
+
+if text_search:
+    st.write(df_search)
+
+
+#  --- JavaScript function to add a new row to the AgGrid table --- 
+js_add_row = JsCode ('''
+function(e) {
+ let api = e.api;
+ let rowPos = e.rowIndex + 1; 
+ api.applyTransaction({addIndex: rowPos, add: [{}]}) 
+};
+'''
+)
+
+#  --- Cell renderer for the '🔧' column to render a button --- 
+cellRenderer_addButton = JsCode('''
+    class BtnCellRenderer {
+        init(params) {
+            this.params = params;
+            this.eGui = document.createElement('div');
+            this.eGui.innerHTML = `
+            <span>
+                <style>
+                .btn_add {
+                    background-color: #71DC87;
+                    border: 2px solid black;
+                    color: #D05732;
+                    text-align: center;
+                    display: inline-block;
+                    font-size: 12px;
+                    font-weight: bold;
+                    height: 2em;
+                    width: 10em;
+                    border-radius: 12px;
+                    padding: 0px;
+                }
+                </style>
+                <button id='click-button' 
+                    class="btn_add" 
+                    >&#x2193; Add</button>
+            </span>
+        `;
+        }
+        getGui() {
+            return this.eGui;
+        }
+    };
+    ''')
+
+#  --- Create a GridOptionsBuilder object from our DataFrame --- 
+gd = GridOptionsBuilder.from_dataframe(df)
+
+# Configure the default column to be editable
+# sets the editable option to True for all columns
+gd.configure_default_column(editable=True)
+
+# Configure the '🔧' column to use our the cell renderer 
+# and onCellClicked function
+gd.configure_column( field = '🔧', 
+                     onCellClicked = js_add_row,
+                     cellRenderer = cellRenderer_addButton,
+                     lockPosition='left')
+
+gridoptions = gd.build()
+
+
+#  --- AgGrid Table with Button Feature --- 
+# Streamlit Form helps from rerunning on every widget-click
+# Also helps in providing layout
+
+with st.form('Inventory') as f:
+    st.header('Inventory List 🔖')
+    
+# Inside the form, we are displaying an AgGrid table using the AgGrid function. 
+# The allow_unsafe_jscode parameter is set to True, 
+# which allows us to use JavaScript code in the AgGrid configuration
+# The theme parameter is set to 'balham', 
+# which applies the Balham theme to the table
+# The height parameter is set to 200, 
+# which specifies the height of the table in pixels.
+# The fit_columns_on_grid_load parameter is set to True, 
+# which ensures that the columns of the table are resized to fit 
+# the width of the table when it is first displayed
+
+    response = AgGrid(df,
+                    gridOptions = gridoptions, 
+                    editable=True,
+                    allow_unsafe_jscode = True, 
+                    theme = 'balham',
+                    height = 200,
+                    fit_columns_on_grid_load = True)
+    st.write(" *Note: Don't forget to hit enter ↩ on new entry.*")
+    st.form_submit_button("Confirm item(s) 🔒", type="primary")
 
