f41477353c5d485431930f7a8216ce78bde10462
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-14 14:26:25 -0500

    Update test2_shopWise_app.py

--- test2_shopWise_app.py
+++ test2_shopWise_app.py
@@ -37,6 +37,7 @@ file_path = "Food_List.csv"
 
 content = read_file(bucket_name, file_path)
 
+AgGrid(content)
 
 #data = pd.read_csv(file_path,
                     #storage_options={'token' : credentials},
@@ -44,9 +45,9 @@ content = read_file(bucket_name, file_path)
                    # index_col=0)
 
 # Print results.
-for line in content.strip().split("\n"):
-    Product_ID, Name , CO2eq_per_Kg , Catagory , Days_in_Pantry , Days_in_Fridge , Days_in_Freezer = line.split(",")
-    st.write(f"{Name} has a :{CO2eq_per_Kg}:")
+#for line in content.strip().split("\n"):
+    #Product_ID, Name , CO2eq_per_Kg , Catagory , Days_in_Pantry , Days_in_Fridge , Days_in_Freezer = line.split(",")
+    #st.write(f"{Name} has a :{CO2eq_per_Kg}:")
     
 #df = pd.read_csv('gs://shopwise-bucket/Food_List.csv')
 #df = pd.DataFrame(content, columns = ['Product_ID','Name','CO2eq_per_Kg','Catagory','Days_in_Pantry','Days_in_Fridge','Days_in_Freezer'])
