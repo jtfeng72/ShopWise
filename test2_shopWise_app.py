44e5f347fa11b8376f0aa940325b9bd9db5d453e
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-13 20:00:27 -0500

    Update test2_shopWise_app.py

--- test2_shopWise_app.py
+++ test2_shopWise_app.py
@@ -43,9 +43,9 @@ st.write(data)
 
 
 # Print results.
-for line in df.strip().split("\n"):
-    Product_ID, Name , CO2eq_per_Kg , Catagory , Days_in_Pantry , Days_in_Fridge , Days_in_Freezer = line.split(",")
-    st.write(f"{Name} has a :{CO2eq_per_Kg}:")
+#for line in df.strip().split("\n"):
+    #Product_ID, Name , CO2eq_per_Kg , Catagory , Days_in_Pantry , Days_in_Fridge , Days_in_Freezer = line.split(",")
+    #st.write(f"{Name} has a :{CO2eq_per_Kg}:")
     
 #df = pd.read_csv('gs://shopwise-bucket/Food_List.csv')
 #df = pd.DataFrame(content, columns = ['Product_ID','Name','CO2eq_per_Kg','Catagory','Days_in_Pantry','Days_in_Fridge','Days_in_Freezer'])
