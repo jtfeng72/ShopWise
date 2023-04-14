ea142832213d41ed2aae9b7c8d43ba54a31d870c
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-14 13:58:08 -0500

    Update test2_shopWise_app.py

--- test2_shopWise_app.py
+++ test2_shopWise_app.py
@@ -44,7 +44,7 @@ content = read_file(bucket_name, file_path)
                    # index_col=0)
 
 # Print results.
-for line in df.strip().split("\n"):
+for line in content.strip().split("\n"):
     Product_ID, Name , CO2eq_per_Kg , Catagory , Days_in_Pantry , Days_in_Fridge , Days_in_Freezer = line.split(",")
     st.write(f"{Name} has a :{CO2eq_per_Kg}:")
     
