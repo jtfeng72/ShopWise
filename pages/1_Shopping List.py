2f0e012d046f4d4e020818332b763ce0ee102b5b
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-16 17:44:34 -0500

    Update 1_Shopping List.py

--- pages/1_Shopping List.py
+++ pages/1_Shopping List.py
@@ -1,4 +1,30 @@
+# ----- Import Library ----- #
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import storage
+import gcsfs
+import pandas as pd
+from io import StringIO 
+import requests
+from st_aggrid import AgGrid, JsCode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+import gspread 
+from gspread_pandas import Spread,Client
+from pysmiles import read_smiles
+import networkx as nx
+import matplotlib.pyplot as plt
+from datetime import datetime
+from pandas import DataFrame
+from collections import namedtuple
+from streamlit_option_menu import option_menu
+import altair as alt
+import math
+from PIL import Image
+# ----- END ----- #
+
+
 # ----- Page setup ----- #
 #st.set_page_config(page_title = "Welcome to ShopWise app", layout= "centered")
 st.title("Welcome to ShopWise app")
+
 # ----- END ----- #
