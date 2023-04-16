afeac0923b9fcbb61c63f2eab4e5b5ec524dbb9e
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-16 17:41:27 -0500

    Update 1_Shopping List.py

--- pages/1_Shopping List.py
+++ pages/1_Shopping List.py
@@ -1,7 +1,4 @@
 # ----- Page setup ----- #
 #st.set_page_config(page_title = "Welcome to ShopWise app", layout= "centered")
 st.title("Welcome to ShopWise app")
-# Display the Logo- Start---- -
-image = Image.open('Shopwise_Logo.png')
-st.image(image, caption='Shop Wisely')
 # ----- END ----- #
