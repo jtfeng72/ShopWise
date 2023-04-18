70a5d0d9017c5d711f279ed80355e61ae698e578
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-17 20:23:27 -0500

    Update and rename shopWise_app.py to shopWise_Home.py

---
+++ shopWise_Home.py
@@ -0,0 +1,17 @@
+# ShopWise.py
+#---Import libraries---#
+from collections import namedtuple
+from streamlit_option_menu import option_menu
+import altair as alt
+import math
+import pandas as pd
+import streamlit as st
+from deta import Deta
+from PIL import Image
+from st_aggrid import AgGrid, JsCode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+
+
+#---Page setup---#
+st.set_page_config(page_title='ShopWise', page_icon=':bar_chart:', layout='wide')
+st.title('Welcome to ShopWise')
