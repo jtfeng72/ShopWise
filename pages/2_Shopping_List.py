9c79721b316ecf96d6871132b0d031b5daa19c22
csun32 <114749799+csun32@users.noreply.github.com>
2023-04-18 14:20:11 -0500

    Update 2_Shopping_List.py

--- pages/2_Shopping_List.py
+++ pages/2_Shopping_List.py
@@ -18,3 +18,51 @@ from pandas import DataFrame
 st.set_page_config(page_title = "ShopWise app",
                    page_icon= "list-task")
 st.title('Shopping list')
+
+# Disable certificate verification (Not necessary always)
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# --- Create a Google Authentication connection objectt --- #
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+
+credentials = service_account.Credentials.from_service_account_info(
+                st.secrets["gcp_service_account"], scopes = scope)
+client = Client(scope=scope,creds=credentials)
+spreadsheetname = "ShopWise Food List"
+spread = Spread(spreadsheetname,client = client)
+
+#st.write(spread.url)
+
+# --- Call the spreadshet --- #
+sh = client.open(spreadsheetname)
+worksheet_list = sh.worksheets()
+
+sheet_id = "1X5ANn3c5UKfpc-P20sMRLJhHggeSaclVfXavdfv-X1c"
+sheet_name = "Pantry"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+Pantry_df = pd.read_csv(url, dtype=str).fillna("")
+
+
+# Functions 
+@st.cache()
+# Get our worksheet names
+def worksheet_names():
+    sheet_names = []   
+    for sheet in worksheet_list:
+        sheet_names.append(sheet.title)  
+    return sheet_names
+
+# Get the sheet as dataframe
+def load_the_spreadsheet(spreadsheetname):
+    worksheet = sh.worksheet(spreadsheetname)
+    df = DataFrame(worksheet.get_all_records())
+    return df
+
+# Update to Sheet
+def update_the_spreadsheet(spreadsheetname,dataframe):
+    col = ['Name','Time_stamp']
+    spread.df_to_sheet(dataframe[col],sheet = spreadsheetname,index = False)
+    st.sidebar.info('Updated to GoogleSheet')
+
