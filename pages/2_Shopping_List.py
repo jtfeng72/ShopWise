f35742ed4a636b84ce4f5182776e1c30da17d91d
csun32 <114749799+csun32@users.noreply.github.com>
2023-04-18 14:23:08 -0500

    Update 2_Shopping_List.py

--- pages/2_Shopping_List.py
+++ pages/2_Shopping_List.py
@@ -1,3 +1,9 @@
+# ShopWise.py
+# ----- Quick note --------#
+
+
+# ----- Entire code below  ------- #
+
 import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import storage
@@ -15,9 +21,9 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 from pandas import DataFrame
 
-st.set_page_config(page_title = "ShopWise app",
-                   page_icon= "list-task")
-st.title('Shopping list')
+# ----- Page setup ----- #
+st.set_page_config(page_title='ShopWise', page_icon=':bar_chart:', layout='wide')
+st.title('Welcome to ShopWise')
 
 # Disable certificate verification (Not necessary always)
 import ssl
@@ -66,6 +72,10 @@ def update_the_spreadsheet(spreadsheetname,dataframe):
     spread.df_to_sheet(dataframe[col],sheet = spreadsheetname,index = False)
     st.sidebar.info('Updated to GoogleSheet')
 
+
+st.header('Food Inventory')
+
+
 # Load data from worksheets
 what_sheets = worksheet_names()
 #st.sidebar.write(what_sheets)
@@ -77,4 +87,126 @@ df = load_the_spreadsheet(ws_choice)
 # Show the availibility as selection
 select_Name = st.sidebar.selectbox('Food_List_Master',list(df['Name']))
 
+
+add = st.sidebar.checkbox('Add New Item')
+if add :  
+    name_entry = st.sidebar.text_input('New Item')
+    confirm_input = st.sidebar.button('Confirm')
+    
+    if confirm_input:
+        now = datetime.now()
+        opt = {'Name': [name_entry],
+              'Time_stamp' :  [now]} 
+        opt_df = DataFrame(opt)
+        df = load_the_spreadsheet('Pantry')
+        new_df = df.append(opt_df,ignore_index=True)
+        update_the_spreadsheet('Pantry',new_df)
+
+
+#  --- JavaScript function to add a new row to the AgGrid table --- 
+js_add_row = JsCode ('''
+function(e) {
+ let api = e.api;
+ let rowPos = e.rowIndex + 1; 
+ api.applyTransaction({addIndex: rowPos, add: [{}]}) 
+};
+'''
+)
+
+#  --- JavaScript function to add a new row to the AgGrid table --- 
+js_add_row = JsCode ('''
+function(e) {
+ let api = e.api;
+ let rowPos = e.rowIndex + 1; 
+ api.applyTransaction({addIndex: rowPos, add: [{}]}) 
+};
+'''
+)
+
+#  --- Cell renderer for the '🔧' column to render a button --- 
+cellRenderer_addButton = JsCode('''
+    class BtnCellRenderer {
+        init(params) {
+            this.params = params;
+            this.eGui = document.createElement('div');
+            this.eGui.innerHTML = `
+            <span>
+                <style>
+                .btn_add {
+                    background-color: #71DC87;
+                    border: 2px solid black;
+                    color: #D05732;
+                    text-align: center;
+                    display: inline-block;
+                    font-size: 12px;
+                    font-weight: bold;
+                    height: 2em;
+                    width: 10em;
+                    border-radius: 12px;
+                    padding: 0px;
+                }
+                </style>
+                <button id='click-button' 
+                    class="btn_add" 
+                    >&#x2193; Add</button>
+            </span>
+        `;
+        }
+        getGui() {
+            return this.eGui;
+        }
+    };
+    ''')         
+
+#  --- Create a GridOptionsBuilder object from our DataFrame --- 
+gd = GridOptionsBuilder.from_dataframe(Pantry_df)
+
+# Configure the default column to be editable
+# sets the editable option to True for all columns
+gd.configure_default_column(editable=True)
+
+# Configure the '🔧' column to use our the cell renderer 
+# and onCellClicked function
+gd.configure_column( field = '🔧', 
+                     onCellClicked = js_add_row,
+                     cellRenderer = cellRenderer_addButton,
+                     lockPosition='left')
+
+gridoptions = gd.build()
+         
+         
+#  --- AgGrid Table with Button Feature --- 
+# Streamlit Form helps from rerunning on every widget-click
+# Also helps in providing layout
+
+with st.form('Inventory') as f:
+    st.header('Inventory List 🔖')
+    
+# Inside the form, we are displaying an AgGrid table using the AgGrid function. 
+# The allow_unsafe_jscode parameter is set to True, 
+# which allows us to use JavaScript code in the AgGrid configuration
+# The theme parameter is set to 'balham', 
+# which applies the Balham theme to the table
+# The height parameter is set to 200, 
+# which specifies the height of the table in pixels.
+# The fit_columns_on_grid_load parameter is set to True, 
+# which ensures that the columns of the table are resized to fit 
+# the width of the table when it is first displayed
+
+    response = AgGrid(Pantry_df,
+                    gridOptions = gridoptions, 
+                    editable=True,
+                    allow_unsafe_jscode = True, 
+                    theme = 'balham',
+                    height = 200,
+                    fit_columns_on_grid_load = True)
+    st.write(" *Note: Don't forget to hit enter ↩ on new entry.*")
+    st.form_submit_button("Confirm item(s) 🔒", type="primary")
+
+
+#  --- Visualize the AgGrid when submit button triggered ---            
+st.subheader("Updated Inventory")
+# Fetch the data from the AgGrid Table
+res = response['data']
+st.table(res) 
 
