70aafb46aac9682c1d22fc7425fbaec7475ffd94
csun32 <114749799+csun32@users.noreply.github.com>
2023-04-18 14:01:54 -0500

    Update 2_Shopping_List.py

--- pages/2_Shopping_List.py
+++ pages/2_Shopping_List.py
@@ -1,1 +1,16 @@
-
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import storage
+import gcsfs
+import pandas as pd
+from io import StringIO 
+import requests
+from st_aggrid import AgGrid, JsCode
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+import gspread 
+from gspread_pandas import Spread,Client
+from pysmiles import read_smiles
+import networkx as nx
+import matplotlib.pyplot as plt
+from datetime import datetime
+from pandas import DataFrame
