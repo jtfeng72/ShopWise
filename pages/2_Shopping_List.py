14b765f5048e8def62a73a27c0b29f84ffc57584
csun32 <114749799+csun32@users.noreply.github.com>
2023-04-19 13:15:52 -0500

    Update 2_Shopping_List.py

--- pages/2_Shopping_List.py
+++ pages/2_Shopping_List.py
@@ -13,11 +13,11 @@ from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 #---Page setup---#
 st.set_page_config(page_title='ShopWise', page_icon=':bar_chart:', layout='wide')
-st.title('Welcome to ShopWise')
+st.title('Shopping List')
 
 #---Connect to the Google Sheet---#
 sheet_id = "1X5ANn3c5UKfpc-P20sMRLJhHggeSaclVfXavdfv-X1c"
-sheet_name = "Food_List_Master"
+sheet_name = "Shopping_List2"
 url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
 df = pd.read_csv(url, dtype=str).fillna("")
 
