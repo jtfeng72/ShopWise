2ee756dfc363e1e1be2f920dba5a6083e1da841d
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-20 10:31:19 -0500

    Update 3_Pantry.py

--- pages/3_Pantry.py
+++ pages/3_Pantry.py
@@ -1,5 +1,4 @@
 # ----- Entire code below  ------- #
-
 import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import storage
@@ -16,6 +15,8 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from datetime import datetime
 from pandas import DataFrame
+from pprint import pprint
+from googleapiclient import discovery
 
 # ----- Page setup ----- #
 st.set_page_config(page_title='ShopWise', page_icon=':bar_chart:', layout='wide')
@@ -25,51 +26,37 @@ option = st.selectbox('Which pantry would you like to access?', ('Email', 'Home 
 st.write('You selected:', option)
 
 
+# ----- Disable certificate verification (Not necessary always) ----- #
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
-"""
-BEFORE RUNNING:
----------------
-1. If not already done, enable the Google Sheets API
-   and check the quota for your project at
-   https://console.developers.google.com/apis/api/sheets
-2. Install the Python client library for Google APIs by running
-   `pip install --upgrade google-api-python-client`
-"""
-from pprint import pprint
 
-from googleapiclient import discovery
+# --- Create a Google Authentication connection objectt --- #
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+credentials = service_account.Credentials.from_service_account_info(
+                st.secrets["gcp_service_account"], scopes = scope)
 
-# TODO: Change placeholder below to generate authentication credentials. See
-# https://developers.google.com/sheets/quickstart/python#step_3_set_up_the_sample
-#
-# Authorize using one of the following scopes:
-#     'https://www.googleapis.com/auth/drive'
-#     'https://www.googleapis.com/auth/drive.file'
-#     'https://www.googleapis.com/auth/drive.readonly'
-#     'https://www.googleapis.com/auth/spreadsheets'
-#     'https://www.googleapis.com/auth/spreadsheets.readonly'
-credentials = None
+gc = gspread.authorize(credentials)
+wks = gc.open("ShopWise Food List").sheet1
+
+
 
 service = discovery.build('sheets', 'v4', credentials=credentials)
 
 # The ID of the spreadsheet to retrieve data from.
-spreadsheet_id = 'my-spreadsheet-id'  # TODO: Update placeholder value.
+spreadsheet_id = "1X5ANn3c5UKfpc-P20sMRLJhHggeSaclVfXavdfv-X1c"  # TODO: Update placeholder value.
 
 # The A1 notation of the values to retrieve.
-range_ = 'my-range'  # TODO: Update placeholder value.
+range_ = 'C2:C23'  # TODO: Update placeholder value.
 
 # How values should be represented in the output.
 # The default render option is ValueRenderOption.FORMATTED_VALUE.
 value_render_option = ''  # TODO: Update placeholder value.
 
-# How dates, times, and durations should be represented in the output.
-# This is ignored if value_render_option is
-# FORMATTED_VALUE.
-# The default dateTime render option is [DateTimeRenderOption.SERIAL_NUMBER].
-date_time_render_option = ''  # TODO: Update placeholder value.
 
-request = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_, valueRenderOption=value_render_option, dateTimeRenderOption=date_time_render_option)
+request = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_, valueRenderOption=value_render_option)
 response = request.execute()
 
 # TODO: Change code below to process the `response` dict:
-pprint(response)
+st.write(response)
