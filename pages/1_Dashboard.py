6391aaa86038fc85ff7ff55dffe043902409efad
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-20 11:12:53 -0500

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -66,7 +66,7 @@ def update_the_shopping_cell():
     wks.update('D7', shopping_option)
     st.sidebar.info('Updated to GoogleSheet')
 
-with st.form():
+with st.form("my_form")):
     shopping_values_list = w.col_values(1)
     shopping_option = st.selectbox('Which shopping list would you like to access?', (shopping_values_list))
     submitted = st.form_submit_button("Confirm")
