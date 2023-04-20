e3ed268b228d33e723c0b732c8a135ad8a3cfd96
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-20 10:42:16 -0500

    Update 3_Pantry.py

--- pages/3_Pantry.py
+++ pages/3_Pantry.py
@@ -15,7 +15,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from datetime import datetime
 from pandas import DataFrame
-from googleapiclient import discovery
+
 
 # ----- Page setup ----- #
 st.set_page_config(page_title='ShopWise', page_icon=':bar_chart:', layout='wide')
