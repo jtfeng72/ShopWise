1c24f135ac6a49d207d66fd016bcbcd21ef0d4fd
csun32 <114749799+csun32@users.noreply.github.com>
2023-04-22 18:54:57 -0500

    Update 2_Shopping_List.py

--- pages/2_Shopping_List.py
+++ pages/2_Shopping_List.py
@@ -136,7 +136,7 @@ with st.form("form"):
                    )
          st.info("Total Rows :" + str(len(grid_table['data'])))
          
-         st.form_submit_button("Confirm Shopping List")
+         submitted = st.form_submit_button("Confirm Shopping List")
     
 if submitted:
          update_the_spreadsheet('Shopping_List2',grid_table["data"]) # update google sheet
