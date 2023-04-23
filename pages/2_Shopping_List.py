25dc8f3b3dc6c0f4132472ab363389823b6f5aa3
csun32 <114749799+csun32@users.noreply.github.com>
2023-04-23 01:08:22 -0500

    Update 2_Shopping_List.py

--- pages/2_Shopping_List.py
+++ pages/2_Shopping_List.py
@@ -146,8 +146,8 @@ with st.form('Shopping List') as f:
          
          submitted = st.form_submit_button("Confirm item(s) 🔒")
          
-         df_final = grid_table["data"].to_dict('dict')
-         st.write(df_final)
+         df_final = grid_table["data"].columns[1:]]
+         df_final
          
          if submitted:
                   update_the_spreadsheet('Shopping_List2',df_final) # update google sheet
