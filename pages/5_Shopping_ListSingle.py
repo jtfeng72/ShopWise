84e1da367b449ba59c31e9452433bf142e16997e
csun32 <114749799+csun32@users.noreply.github.com>
2023-04-24 20:25:41 -0500

    Update 5_Shopping_ListSingle.py

--- pages/5_Shopping_ListSingle.py
+++ pages/5_Shopping_ListSingle.py
@@ -1,21 +1,5 @@
-from google.oauth2 import service_account
-from gspread_pandas import Spread,Client
-import pandas as pd
+
 import streamlit as st
-import gspread 
-from st_aggrid import AgGrid, GridUpdateMode, JsCode
-from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 st.title('Shopping List')
-# --- Create a Google Authentication connection objectt --- #
-scope = ['https://spreadsheets.google.com/feeds',
-         'https://www.googleapis.com/auth/drive']
-credentials = service_account.Credentials.from_service_account_info(
-                st.secrets["gcp_service_account"], scopes = scope)
-gc = gspread.authorize(credentials)
 
-# --- Get List Value and make drop down --- #
-# open your spreadsheet
-s = gc.open("ShopWise Food List") 
-# and worksheet
-w = s.worksheet("shopping_list2") #get data from dropbox tab
