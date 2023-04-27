ca076f248647b2dc756742df260ab7c0628c4324
kluuking <71826000+kluuking@users.noreply.github.com>
2023-04-27 14:37:49 -0500

    Update ShopWise_Home.py

--- ShopWise_Home.py
+++ ShopWise_Home.py
@@ -9,6 +9,7 @@ import streamlit as st
 from PIL import Image
 from st_aggrid import AgGrid, JsCode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
+import streamlit as st
 
 
 #---Page setup---#
@@ -19,3 +20,11 @@ st.title('Welcome to ShopWise')
 image = Image.open('Shopwise_Logo.png')
 st.image(image, caption='Shop Wisely')
 # ----- END ----- #
+
+
+#------Start- OF video-----#
+video_file = open('Shopwise.mp4', 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)
+#-------End-------#
