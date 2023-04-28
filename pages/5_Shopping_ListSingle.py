373539fa9ef5477c25862878223e5720e1a13d17
csun32 <114749799+csun32@users.noreply.github.com>
2023-04-27 20:33:46 -0500

    Update 5_Shopping_ListSingle.py

--- pages/5_Shopping_ListSingle.py
+++ pages/5_Shopping_ListSingle.py
@@ -4,6 +4,7 @@ from google.oauth2 import service_account                      #pip install goog
 from gspread_pandas import Spread,Client                       #pip install gspread_pandas
 import pandas as pd                                            #pip install pandas
 import streamlit as st                                         #pip install streamlit
+from streamlit_option_menu import option_menu
 import gspread                                                 #pip install gspread
 from st_aggrid import AgGrid, GridUpdateMode, JsCode           #pip install streamlit-aggrid
 from st_aggrid.grid_options_builder import GridOptionsBuilder
