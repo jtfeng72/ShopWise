3442ff592953b479220179ccfb4b3df3cb3ccb72
kluuking <71826000+kluuking@users.noreply.github.com>
2023-04-28 19:31:36 -0500

    Update ShopWise_Home.py

--- ShopWise_Home.py
+++ ShopWise_Home.py
@@ -21,7 +21,7 @@ st.write("ShopWise")
 col1, mid, col2, = st.beta_columns([1,1,25], gap="large")
 with col1:
     st.image('Shopwise_logo2.png', width=300)
-with col3:
+with col2:
     st.write("Welcome to ShopWise, your one-stop-shop for all things green and sustainable! Our app is designed to make eco-friendly shopping easier and more accessible than ever before. We understand that it can be overwhelming to navigate the world of green shopping, which is why we've curated a selection of products that meet our high standards for sustainability. From clothing and home goods to food and personal care items, we've got you covered. With ShopWise, you can trust that every purchase you make is making a positive impact on the environment. We believe that small choices can make a big difference, and we're excited to help you make those choices every day. So join us in our mission to create a healthier planet, one shopping trip at a time. Download ShopWise today and start shopping with purpose!")
 
 # Display the Logo- Start---- -
