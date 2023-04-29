8d92f38b8bbcd598e8b4cd56cd2691119eec3eda
kluuking <71826000+kluuking@users.noreply.github.com>
2023-04-28 19:33:41 -0500

    Update ShopWise_Home.py

--- ShopWise_Home.py
+++ ShopWise_Home.py
@@ -17,7 +17,7 @@ st.set_page_config(page_title='ShopWise', page_icon=':bar_chart:', layout='wide'
 st.title('Welcome to ShopWise')
 #---------------#
 
-st.write("ShopWise")
+st.write("Made by Group 4")
 col1, mid, col2, = st.beta_columns([1,1,13], gap="large")
 with col1:
     st.image('Shopwise_logo2.png', width=300)
@@ -25,8 +25,8 @@ with col2:
     st.write("Welcome to ShopWise, your one-stop-shop for all things green and sustainable! Our app is designed to make eco-friendly shopping easier and more accessible than ever before. We understand that it can be overwhelming to navigate the world of green shopping, which is why we've curated a selection of products that meet our high standards for sustainability. From clothing and home goods to food and personal care items, we've got you covered. With ShopWise, you can trust that every purchase you make is making a positive impact on the environment. We believe that small choices can make a big difference, and we're excited to help you make those choices every day. So join us in our mission to create a healthier planet, one shopping trip at a time. Download ShopWise today and start shopping with purpose!")
 
 # Display the Logo- Start---- -
-image = Image.open('Shopwise_logo2.png')
-st.image(image, caption='Shop Wisely')
+#image = Image.open('Shopwise_logo2.png')
+#st.image(image, caption='Shop Wisely')
 # ----- END ----- #
 st.write("Welcome to ShopWise, your one-stop-shop for all things green and sustainable! Our app is designed to make eco-friendly shopping easier and more accessible than ever before. We understand that it can be overwhelming to navigate the world of green shopping, which is why we've curated a selection of products that meet our high standards for sustainability. From clothing and home goods to food and personal care items, we've got you covered. With ShopWise, you can trust that every purchase you make is making a positive impact on the environment. We believe that small choices can make a big difference, and we're excited to help you make those choices every day. So join us in our mission to create a healthier planet, one shopping trip at a time. Download ShopWise today and start shopping with purpose!")
 
