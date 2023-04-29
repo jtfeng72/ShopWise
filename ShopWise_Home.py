b7ca461dfe070dcbbce04a3e4328c362087b0214
kluuking <71826000+kluuking@users.noreply.github.com>
2023-04-28 19:34:25 -0500

    Update ShopWise_Home.py

--- ShopWise_Home.py
+++ ShopWise_Home.py
@@ -28,11 +28,11 @@ with col2:
 #image = Image.open('Shopwise_logo2.png')
 #st.image(image, caption='Shop Wisely')
 # ----- END ----- #
-st.write("Welcome to ShopWise, your one-stop-shop for all things green and sustainable! Our app is designed to make eco-friendly shopping easier and more accessible than ever before. We understand that it can be overwhelming to navigate the world of green shopping, which is why we've curated a selection of products that meet our high standards for sustainability. From clothing and home goods to food and personal care items, we've got you covered. With ShopWise, you can trust that every purchase you make is making a positive impact on the environment. We believe that small choices can make a big difference, and we're excited to help you make those choices every day. So join us in our mission to create a healthier planet, one shopping trip at a time. Download ShopWise today and start shopping with purpose!")
+#st.write("Welcome to ShopWise, your one-stop-shop for all things green and sustainable! Our app is designed to make eco-friendly shopping easier and more accessible than ever before. We understand that it can be overwhelming to navigate the world of green shopping, which is why we've curated a selection of products that meet our high standards for sustainability. From clothing and home goods to food and personal care items, we've got you covered. With ShopWise, you can trust that every purchase you make is making a positive impact on the environment. We believe that small choices can make a big difference, and we're excited to help you make those choices every day. So join us in our mission to create a healthier planet, one shopping trip at a time. Download ShopWise today and start shopping with purpose!")
 
 # Display the Fork Image- Start---#
-image = Image.open('GlobeFork.png')
-st.image(image, caption='Eat Green')
+#image = Image.open('GlobeFork.png')
+#st.image(image, caption='Eat Green')
 # ----- END ----- #
 
 #------Start- OF video-----#
