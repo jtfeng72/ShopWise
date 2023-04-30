d53b4020b609af037bad17acc88289c93d2e8bc2
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-29 23:32:39 -0500

    Update 3_Pantry V2.py

--- pages/3_Pantry V2.py
+++ pages/3_Pantry V2.py
@@ -106,7 +106,6 @@ if add_submitted:
          update_annotated_spreadsheet('Pantry',annotated) # update google sheet
          df = load_the_spreadsheet(sheet_name) 
 elif add_Refreshd:
-         update_annotated_spreadsheet('Pantry',annotated) # update google sheet
          df = load_the_spreadsheet(sheet_name) 
 else:
      st.write('Incorrect')
