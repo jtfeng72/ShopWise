c622a94c7a959a30e566d9c2b776f6c4e0d30ce1
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-29 23:14:12 -0500

    Update 3_Pantry V2.py

--- pages/3_Pantry V2.py
+++ pages/3_Pantry V2.py
@@ -105,12 +105,15 @@ add_Refreshd = st.button("Refresh")
 if add_submitted:
          update_annotated_spreadsheet('Pantry',annotated) # update google sheet
          df = load_the_spreadsheet(sheet_name) 
-elif add_Refreshd:
-         update_annotated_spreadsheet('Pantry',annotated) # update google sheet
-         df = load_the_spreadsheet(sheet_name) 
+         df = load_the_spreadsheet(sheet_name)  
 else:
      st.write('Incorrect')
 
+"""
+elif add_Refreshd:
+         update_annotated_spreadsheet('Pantry',annotated) # update google sheet
+         df = load_the_spreadsheet(sheet_name) 
+"""
   
 # ----- Select pantry ----- #
 df = load_the_spreadsheet(sheet_name)
