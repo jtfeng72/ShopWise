9c23bc3691b070c48ec599052192510dd98435b3
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-04-30 12:11:42 -0500

    Update 1_Dashboard.py

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -1,21 +1,17 @@
-# ShopWise.py
-# ----- Quick note --------#
+#ShopWise Dashboard.py
+# ----- Entire code below  ------- #  
 
-
-# ----- Entire code below  ------- #
-
+# ----- Import libraries  ------- #  
 import streamlit as st
 from google.oauth2 import service_account
 from google.cloud import storage
 import gcsfs
 import pandas as pd
-from io import StringIO 
 import requests
 from st_aggrid import AgGrid, JsCode
 from st_aggrid.grid_options_builder import GridOptionsBuilder
 import gspread 
 from gspread_pandas import Spread,Client
-import networkx as nx
 import matplotlib.pyplot as plt
 from datetime import datetime
 from pandas import DataFrame
@@ -24,25 +20,38 @@ from pandas import DataFrame
 st.set_page_config(page_title='ShopWise', page_icon=':bar_chart:', layout='wide')
 st.title('Welcome to ShopWise')
 
-# Disable certificate verification (Not necessary always)
+
+# ----- Disable certificate verification (Not necessary always) ----- #
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-# --- Create a Google Authentication connection objectt --- #
 
-#---------------------------------------------------------
+# ----- Create a Google Authentication connection objectt ----- #
 scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/drive']
 credentials = service_account.Credentials.from_service_account_info(
                 st.secrets["gcp_service_account"], scopes = scope)
-
-gc = gspread.authorize(credentials)
-wks = gc.open("ShopWise Food List").sheet1
+client = Client(scope=scope,creds=credentials)
 
 
-# Update to user cell
+# ----- Connect to the Google Shee ----- #
+spreadsheetname = "ShopWise Food List"
+spread = Spread(spreadsheetname,client = client)
+sh = client.open(spreadsheetname)
+
+sheet_id = "1X5ANn3c5UKfpc-P20sMRLJhHggeSaclVfXavdfv-X1c"
+sheet_name = "Pantry"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+p_df = pd.read_csv(url, dtype=str).fillna("")
+
+sheet_name2 = "Food_List_Master"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name2}"
+fd_list_df = pd.read_csv(url, dtype=str).fillna("")
+
+
+# ----- Creating functions ----- #
 def update_the_user_cell():
-    wks.update('C5', username)
+    sh.update('C5', username)
     st.sidebar.info('Updated to GoogleSheet')
 
 with st.form("form"):
@@ -55,7 +64,7 @@ with st.form("form"):
 
 # --- Get List Value and make drop down --- #
 # open your spreadsheet
-s = gc.open("ShopWise Food List") 
+s = spread.open("ShopWise Food List") 
 # and worksheet
 w = s.worksheet("DropBox")
 
