9359c98f466cf90aed433ac546ce13214b1d9744
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-01 22:40:23 -0500

    Update 5_Recomendation.py

--- pages/5_Recomendation.py
+++ pages/5_Recomendation.py
@@ -1,1 +1,13 @@
+#ShopWise.py
+#---Import libraries---#
+from google.oauth2 import service_account                      #pip install google-auth
+from gspread_pandas import Spread,Client                       #pip install gspread_pandas
+import pandas as pd                                            #pip install pandas
+import streamlit as st                                         #pip install streamlit
+from streamlit_option_menu import option_menu
+import gspread                                                 #pip install gspread
+from st_aggrid import AgGrid, GridUpdateMode, JsCode           #pip install streamlit-aggrid
+from st_aggrid.grid_options_builder import GridOptionsBuilder
+
+st.title('Welcome to your shopping list') #Page Title
 
