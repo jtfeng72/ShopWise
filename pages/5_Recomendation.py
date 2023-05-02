5bafa073ab8c3363dae3af5a051abb59dad3634c
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-01 22:53:32 -0500

    Update 5_Recomendation.py

--- pages/5_Recomendation.py
+++ pages/5_Recomendation.py
@@ -11,3 +11,21 @@ from st_aggrid.grid_options_builder import GridOptionsBuilder
 
 st.title('Welcome to your shopping list') #Page Title
 
+def load_the_spreadsheet(spreadsheetname):
+    # --- Create a Google Authentication connection objectt --- #
+    scope = ['https://spreadsheets.google.com/feeds',
+             'https://www.googleapis.com/auth/drive']
+    credentials = service_account.Credentials.from_service_account_info(
+                    st.secrets["gcp_service_account"], scopes = scope)
+
+    client = Client(scope=scope,creds=credentials)
+    spreadsheetname = "ShopWise Food List"                #spreadsheet name
+    #spread = Spread(spreadsheetname,client = client)      #load ShopWise Food List google sheet
+    # --- Call the spreadshet --- #
+    sh = client.open(spreadsheetname)                     #load ShopWise Food List google sheet
+    #worksheet_list = sh.worksheets()                      # list of ALL worksheets in the google sheet <Worksheet 'Shopping_List2' id:986753546>...
+    worksheet = sh.worksheet(spreadsheetname)
+    df = pd.DataFrame(worksheet.get_all_records())
+    return df
+
+st.dataframe(load_the_spreadsheet("Pantry"))
