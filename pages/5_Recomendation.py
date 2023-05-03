d347a43a2fc47ade4ea2de52a099457a481f18fa
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-02 19:31:48 -0500

    Update 5_Recomendation.py

--- pages/5_Recomendation.py
+++ pages/5_Recomendation.py
@@ -33,14 +33,14 @@ df=load_the_spreadsheet("Pantry")
 
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
-Status = st.sidebar.multiselect(
+status = st.sidebar.multiselect(
     "Select your grocery status:",
     options=df["Status"].unique(),
     default=df["Status"].unique()               #prepopulate all status
 )
 
 df_selection = df.query(
-    "Status == @Status"
+    "Status == @status"
 )
 
 st.dataframe(df)
