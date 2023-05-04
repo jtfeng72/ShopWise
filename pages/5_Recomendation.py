34edccedb682816906d61e1a3b76e164c1be9447
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-04 16:34:27 -0500

    Update 5_Recomendation.py

--- pages/5_Recomendation.py
+++ pages/5_Recomendation.py
@@ -38,9 +38,9 @@ df_c2= df_c.merge(fd_list,
                   left_on= 'Item',
                   right_on= 'Name',
                   how = 'left')
-df_3 = df_c2[["Item", "Weight", "Storage", "Purchase_Date'","Consumed","Wasted","CO2_Per_g","Expiration","Expiration_Date"]]
+#df_3 = df_c2[["Item", "Weight", "Storage", "Purchase_Date'","Consumed","Wasted","CO2_Per_g","Expiration","Expiration_Date"]]
 
-st.dataframe[df_3]
+st.dataframe[df_2]
 
 
 
