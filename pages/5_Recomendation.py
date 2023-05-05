0b207607aa69de0baa6f335eb0ab88ce54501979
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-05 15:43:05 -0500

    Update 5_Recomendation.py

--- pages/5_Recomendation.py
+++ pages/5_Recomendation.py
@@ -113,7 +113,7 @@ emis_by_mth = (
 )
 fig_emis_by_mth = px.bar(
     emis_by_mth,
-    x="P_Month",
+    x=emis_by_mth.index,
     y="Emission",
     title="<b>Waste Emission by Month</b>",
     color_discrete_sequence=["#0083B8"] * len(emis_by_mth),
