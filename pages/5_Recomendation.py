36825445c57c85aa2c93cf644bdba6d38ac69304
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-05 11:42:26 -0500

    Update 5_Recomendation.py

--- pages/5_Recomendation.py
+++ pages/5_Recomendation.py
@@ -89,6 +89,18 @@ st.dataframe(df_selection)
 emis_by_cat = (
     df_selection.groupby(by=["Category"]).sum()[["Emission"]].sort_values(by="Emission")
 )
-
+fig_emis_by_cat = px.bar(
+    emis_by_cat,
+    x="Total",
+    y=emis_by_cat.index,
+    orientation="h",
+    title="<b>Waste Emission by Category</b>",
+    color_discrete_sequence=["#0083B8"] * len(emis_by_cat),
+    template="plotly_white",
+)
+fig_emis_by_cat.update_layout(
+    plot_bgcolor="rgba(0,0,0,0)",
+    xaxis=(dict(showgrid=False))
+)
 
 
