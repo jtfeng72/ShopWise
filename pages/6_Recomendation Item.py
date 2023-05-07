81225fb503ecb2e4952030f3f469767757f5f160
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-05-07 01:57:13 -0500

    Update 6_Recomendation Item.py

--- pages/6_Recomendation Item.py
+++ pages/6_Recomendation Item.py
@@ -43,7 +43,7 @@ dashboard = sh.worksheet("Dashboard V2")
 
 sheet_id = "1X5ANn3c5UKfpc-P20sMRLJhHggeSaclVfXavdfv-X1c"
 d_rec = "Dashboard_Rec"
-url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={dashboard_pantry}"
+url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={d_rec}"
 dashr = pd.read_csv(url, dtype=str).fillna("")
 
 
@@ -55,20 +55,26 @@ def load_the_spreadsheet(spreadsheetname):
     return df
 
 def update_the_status_cell():
-    dashboard.update('I4', selstatus)
+    dashboard.update('T4', option)
+    dashboard.update('V4', str_date)
+    dashboard.update('W4', end_date)
     st.sidebar.info('Updated to GoogleSheet')
 
 
-# ----- Pantry Dashboard ----- #
-st.markdown('Pantry')
+# ----- Rec Dashboard ----- #
+st.markdown('Item Recomendation')
 
 with st.form("form"):
-    selstatus = st.radio('Select Status:',['In Progress','Completed'])
+    option = st.selectbox(
+...     'Item Category?',
+...     ('Meat', 'Dairy', 'Processed Agricultural Products', 'Non-Processed Agricultural Products', 'Seafood'))
+    str_date = st.date_input("Start Date")
+    end_date = st.date_input("End Date")
     submitted = st.form_submit_button("Confirm")
     
     if submitted:
         update_the_status_cell()
 
-dashp=load_the_spreadsheet(dashboard_pantry)
+dashr=load_the_spreadsheet(d_rec)
 
-AgGrid(dashp)
+AgGrid(dashr)
