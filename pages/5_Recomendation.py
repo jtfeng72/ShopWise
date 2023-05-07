67c9924a57f482cd594fcab4b978e0ffb332cba3
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-07 09:09:51 -0500

    Update 5_Recomendation.py

--- pages/5_Recomendation.py
+++ pages/5_Recomendation.py
@@ -76,7 +76,7 @@ df_selection = df_c2.query(
 
 st.title(':bar_chart: Here are your grocery stats') #Page Title
 st.markdown("##")
-total_waste = round(df_selection['Wasted'].sum()/1000,2)
+total_waste = round(df_selection['Wasted'].sum(),2)
 total_emission = round(df_selection['Emission'].sum(),2)
 left_column, right_column = st.columns(2)
 
@@ -86,6 +86,7 @@ with right_column:
     st.subheader(f"Total Emissions: {total_emission:,} kgCO2eq")
     
 st.markdown("""---""")
+'''The results reflects to the filter to the left'''
 
 #st.dataframe(df_selection)
 
