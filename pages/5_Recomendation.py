7e439a4827354b6a2491b7c0311fee763be62dbd
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-07 12:09:19 -0500

    Update 5_Recomendation.py

--- pages/5_Recomendation.py
+++ pages/5_Recomendation.py
@@ -4,10 +4,7 @@ from google.oauth2 import service_account                      #pip install goog
 from gspread_pandas import Spread,Client                       #pip install gspread_pandas
 import pandas as pd                                            #pip install pandas
 import streamlit as st                                         #pip install streamlit
-from streamlit_option_menu import option_menu
 import gspread                                                 #pip install gspread
-from st_aggrid import AgGrid, GridUpdateMode, JsCode           #pip install streamlit-aggrid
-from st_aggrid.grid_options_builder import GridOptionsBuilder
 import plotly.express as px                                     #pip install plotly-express
 from datetime import datetime, date
 
