c5b505be7186c281f731fb099daf4155351f7f53
Kevin Feng <62178163+jtfeng72@users.noreply.github.com>
2023-05-08 21:30:47 -0500

    Update 6_Recommended Items.py

--- pages/6_Recommended Items.py
+++ pages/6_Recommended Items.py
@@ -68,8 +68,8 @@ st.markdown('Item Recomendation')
 
 with st.form("form"):
     option = st.selectbox("Item Category?",('Meat', 'Dairy', 'Processed Agricultural Products', 'Non-Processed Agricultural Products', 'Seafood'))
-    str_date = st.date_input("Start Date",datetime.date())
-    end_date = st.date_input("End Date",datetime.date())
+    str_date = st.date_input("Start Date",datetime.date)
+    end_date = st.date_input("End Date",datetime.date)
     submitted = st.form_submit_button("Confirm")
     
     if submitted:
