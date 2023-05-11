1010cf33998647ba9ed0d68a8bddf9b7bfb36b7b
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-11 13:32:13 -0500

    Update 5_Recomendation.py

--- pages/5_Recomendation.py
+++ pages/5_Recomendation.py
@@ -47,7 +47,7 @@ df_c2['Emission']= df_c2['Wasted'] * df_c2['CO2_Per_g']                         
 df_c2["Purchase_Date"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%Y-%m-%d')                # Change to date type
 df_c2["Month"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%B')                              # New column to extract month
 df_c2["Year"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.year                                         # New column to extract year
-df_c2["Year_Month"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.to_period('M')                         # New column to extract Year Month
+df_c2["Year_Month"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%Y-%m')                   # New column to extract Year Month
 #st.dataframe(df_c2)
 
 
