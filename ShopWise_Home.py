cd4c9e7e1be73535e978f093229685add5a1d017
kluuking <71826000+kluuking@users.noreply.github.com>
2023-05-10 19:29:14 -0500

    Update ShopWise_Home.py

--- ShopWise_Home.py
+++ ShopWise_Home.py
@@ -18,10 +18,7 @@ st.title('Welcome to ShopWise')
 #---------------#
 
 st.write("Made by Group 4")
-col1, mid, col2, = st.beta_columns([1,1,8], gap="large")
-with col1:
-with col2:
-    st.write("Welcome to ShopWise, your one-stop-shop for all things green and sustainable! Our app is designed to make eco-friendly shopping easier and more accessible than ever before, by wisely recommending a sustainable ammount to purchase. We understand that it can be overwhelming to navigate the world of green shopping, which is why we've curated a selection of products that meet our high standards for sustainability. With ShopWise, you can trust that every purchase you make is making a positive impact on the environment. We believe that small choices can make a big difference, and we're excited to help you make those choices every day. So join us in our mission to create a healthier planet, one shopping trip at a time. Download ShopWise today and start shopping with purpose!")
+st.write("Welcome to ShopWise, your one-stop-shop for all things green and sustainable! Our app is designed to make eco-friendly shopping easier and more accessible than ever before, by wisely recommending a sustainable ammount to purchase. We understand that it can be overwhelming to navigate the world of green shopping, which is why we've curated a selection of products that meet our high standards for sustainability. With ShopWise, you can trust that every purchase you make is making a positive impact on the environment. We believe that small choices can make a big difference, and we're excited to help you make those choices every day. So join us in our mission to create a healthier planet, one shopping trip at a time. Download ShopWise today and start shopping with purpose!")
 
 #------Start- OF video-----#
 video_file = open('ShopWise Introduction.mp4', 'rb')
