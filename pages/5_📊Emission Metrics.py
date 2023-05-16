ce1ce4d9d0389ba3b8d1e24bf9164a9db43cd2a9
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-15 21:31:25 -0500

    Update 5_📊Emission Metrics.py

--- pages/5_📊Emission Metrics.py
+++ pages/5_📊Emission Metrics.py
@@ -82,17 +82,22 @@ st.markdown("##")
 total_waste = round(df_selection['Wasted'].sum()/1000,2)
 total_emission = round(df_selection['Emission'].sum()/1000,2)
 left_column, right_column = st.columns(2)
-
-em_by_prd_df = df_selection.groupby(by=["Year_Month"]).sum()[["Emission"]]
-current_em = em_by_prd_df[(em_by_prd_df.index == current_prd)].values[0][0]
-prior_em = em_by_prd_df[(em_by_prd_df.index == prior_prd)].values[0][0]
-em_change = current_em - prior_em
+    
 
 with left_column:
     st.subheader(f"Total Waste: {total_waste:,} kg")
     st.subheader(f"Total Emissions: {total_emission:,} kgCO2eq")
 with right_column:
+    # streamlit metric wiget variable
+em_by_prd_df = df_c2.groupby(by=["Year_Month"]).sum()[["Emission"]]                 #summarize emission by month
+if len(em_by_prd_df) >1:
+    current_em = em_by_prd_df[(em_by_prd_df.index == current_prd)].values[0][0]
+    prior_em = em_by_prd_df[(em_by_prd_df.index == prior_prd)].values[0][0]
+    em_change = current_em - prior_em
     st.metric(label="Current Month Emission", value = f"{round(current_em/1000,2)} kgCO2eq", delta =  f"{round(em_change/prior_em*100,1)} %",
+    delta_color="inverse")
+else:
+    st.metric(label="Current Month Emission", value = "Pior month is not avliable", delta =  "NA %",
     delta_color="inverse")
     
 st.markdown("""---""")
