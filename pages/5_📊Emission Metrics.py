24c6ec0724f5a46d83ac29991cec905cef198cc7
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-18 17:56:49 -0500

    Update 5_📊Emission Metrics.py

--- pages/5_📊Emission Metrics.py
+++ pages/5_📊Emission Metrics.py
@@ -95,11 +95,11 @@ elif any(df.Status.unique() == 'Completed'):
         st.subheader(f"Total Emissions: {total_emission:,} kgCO2eq")
     with right_column:
         # streamlit metric wiget variable
-        em_by_prd_df = df_c2.groupby(by=["Year_Month"]).sum()[["Emission"]]
-        #df_c2.groupby('Year_Month')['Emission'].sum()                    #summarize total emission by month
+        em_by_prd_df = df_c2.groupby('Year_Month')['Emission'].sum()                    #summarize total emission by month
+        #df_c2.groupby(by=["Year_Month"]).sum()[["Emission"]]
         st.dataframe(em_by_prd_df)
         if len(em_by_prd_df) >1:                                                                #show metric only if there is 2 or more month of data
-            current_em = em_by_prd_df[(em_by_prd_df.index == current_prd)].values[0][0]         #current month emission
+            current_em = em_by_prd_df[(em_by_prd_df['Year_Month'] == current_prd)].values[0][0]         #current month emission
             prior_em = em_by_prd_df[(em_by_prd_df.index == prior_prd)].values[0][0]             #prior month emission
             em_change = current_em - prior_em                                                   #the difference between current and pror month emission
             #metric wigget
