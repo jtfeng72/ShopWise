b276f1b9b0d1a42d210bf90229f81c6ce968ce44
kluuking <71826000+kluuking@users.noreply.github.com>
2023-05-17 20:52:36 -0500

    Update ShopWise_Home.py

--- ShopWise_Home.py
+++ ShopWise_Home.py
@@ -17,7 +17,7 @@ st.set_page_config(page_title='ShopWise', page_icon=':bar_chart:', layout='cente
 st.title('Welcome to ShopWise')
 #---End of Page setup---#
 
-st.write("Made by Group 4")
+st.write("Made by Kevin Luu, Kevin Feng, Andy Sun")
 st.write("Welcome to ShopWise, your one-stop-shop for all things green and sustainable! Our app is designed to make eco-friendly shopping easier and more accessible than ever before, by wisely recommending a sustainable ammount to purchase. We understand that it can be overwhelming to navigate the world of green shopping, which is why we've curated a selection of products that meet our high standards for sustainability. With ShopWise, you can trust that every purchase you make is making a positive impact on the environment. We believe that small choices can make a big difference, and we're excited to help you make those choices every day. So join us in our mission to create a healthier planet, one shopping trip at a time. Download ShopWise today and start shopping with purpose!")
 
 #------Start- OF video-----#
