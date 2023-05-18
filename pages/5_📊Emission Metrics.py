21cbafc2828b737b23c006dd607ea07534ce9bd5
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-18 16:48:09 -0500

    Update 5_📊Emission Metrics.py

--- pages/5_📊Emission Metrics.py
+++ pages/5_📊Emission Metrics.py
@@ -53,7 +53,7 @@ elif any(df.Status.unique() == 'Completed'):
     df_c2["Purchase_Date"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%Y-%m-%d')                # Change to date type
     df_c2["Month"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%B')                              # New column to extract month
     df_c2["Year"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.year                                         # New column to extract year
-    df_c2["Year_Month"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%Y%m')                   # New column to extract Year Month
+    df_c2["Year_Month"] = int(pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%Y%m'))                   # New column to extract Year Month
     #st.dataframe(df_c2)
 
 
