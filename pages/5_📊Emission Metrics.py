f3023556c3c00e8fd84e23aa7302929365181869
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-18 18:14:26 -0500

    Update 5_📊Emission Metrics.py

--- pages/5_📊Emission Metrics.py
+++ pages/5_📊Emission Metrics.py
@@ -137,7 +137,8 @@ elif any(df.Status.unique() == 'Completed'):
 
     # emission by month
     emis_by_mth = (
-        df_selection.groupby(by=["Month"]).sum()[["Emission"]]
+        df_selection.groupby('Month')['Emission']
+        #df_selection.groupby(by=["Month"]).sum()[["Emission"]]
     )
     fig_emis_by_mth = px.bar(
         emis_by_mth,
