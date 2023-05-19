6a3cc4c62574a8cc86b81a1f3e5a15ec6c22c233
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-18 23:19:40 -0500

    Update 7_📊Emission Metrics2.py

--- pages/7_📊Emission Metrics2.py
+++ pages/7_📊Emission Metrics2.py
@@ -102,8 +102,8 @@ elif any(df.Status.unique() == 'Completed'):
         em_by_prd_df = df_c2.groupby(by=["Year_Month"])["Emission"].sum()                    #summarize total emission by month
         #st.write(len(em_by_prd_df))
         if len(em_by_prd_df) >1:                                                                #show metric only if there is 2 or more month of data
-            current_em = em_by_prd_df[(em_by_prd_df.index == current_prd)].values[0][0]         #current month emission
-            prior_em = em_by_prd_df[(em_by_prd_df.index == prior_prd)].values[0][0]             #prior month emission
+            current_em = em_by_prd_df[(em_by_prd_df.index == current_prd)].values[0]         #current month emission
+            prior_em = em_by_prd_df[(em_by_prd_df.index == prior_prd)].values[0]             #prior month emission
             em_change = current_em - prior_em                                                   #the difference between current and pror month emission
             #metric wigget
             st.metric(label="Current Month Emission", value = f"{round(current_em/1000,2)} kgCO2eq", delta =  f"{round(em_change/prior_em*100,1)} %",
