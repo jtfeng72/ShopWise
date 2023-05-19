f0935bf1711430bf941cd6f0fcabb26e02795c27
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-18 23:54:40 -0500

    Update 5_📊Emission Metrics.py

--- pages/5_📊Emission Metrics.py
+++ pages/5_📊Emission Metrics.py
@@ -52,7 +52,7 @@ elif any(df.Status.unique() == 'Completed'):
     df_c2["Purchase_Date"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%Y-%m-%d')                # Change to date type
     df_c2["Month"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%b')                              # New column to extract month
     df_c2["Year"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.year                                         # New column to extract year
-    df_c2["Year_Month"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%Y-%m')                   # New column to extract Year Month
+    df_c2["Year_Month"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%y%m')                   # New column to extract Year Month
     st.write(df_c2.dtypes)
     st.dataframe(df_c2)
 
@@ -78,10 +78,10 @@ elif any(df.Status.unique() == 'Completed'):
 
     #The summary of total Waste and Emission
     today = datetime.today()
-    current_prd = today.strftime('%Y-%m')
+    current_prd = today.strftime('%y%m')
     first = today.replace(day=1)
     last_month = first - timedelta(days=1)
-    prior_prd = last_month.strftime('%Y-%m')
+    prior_prd = last_month.strftime('%y%m')
 
     st.title('📊Emission Metrics')                                          #Page Title
     st.markdown("##")
