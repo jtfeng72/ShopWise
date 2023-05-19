671a94f1215e8c1111d3c93511755839ac4df2f6
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-18 23:38:16 -0500

    Update 5_📊Emission Metrics.py

--- pages/5_📊Emission Metrics.py
+++ pages/5_📊Emission Metrics.py
@@ -47,6 +47,7 @@ elif any(df.Status.unique() == 'Completed'):
 
     #st.write(df.dtypes) #to check data type
     #Dataframe modification
+    df_c2['Wasted']= df_c2['Wasted'].astype(float)
     df_c2['Emission']= df_c2['Wasted'] * df_c2['CO2_Per_g']                                                # Calculating Emission
     df_c2["Purchase_Date"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%Y-%m-%d')                # Change to date type
     df_c2["Month"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%b')                              # New column to extract month
