f21bb297989000d751cf1105b77f37aa676ede40
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-18 22:49:30 -0500

    Update 7_📊Emission Metrics2.py

--- pages/7_📊Emission Metrics2.py
+++ pages/7_📊Emission Metrics2.py
@@ -52,9 +52,9 @@ elif any(df.Status.unique() == 'Completed'):
     df_c2['Wasted']= df_c2['Wasted'].astype(int)
     df_c2['Emission']= df_c2['Wasted'] * df_c2['CO2_Per_g']                                               # Calculating Emission
     df_c2["Purchase_Date"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%Y-%m-%d')                # Change to date type
-    df_c2["Month"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%B')                              # New column to extract month
+    df_c2["Month"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%b')                              # New column to extract month
     df_c2["Year"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.year                                         # New column to extract year
-    df_c2["Year_Month"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%Y%m')                   # New column to extract Year Month
+    df_c2["Year_Month"] = pd.to_datetime(df_c2["Purchase_Date"]).dt.strftime('%y-%m')                   # New column to extract Year Month
     
     st.write(df_c2.dtypes)
     st.dataframe(df_c2)
@@ -81,10 +81,10 @@ elif any(df.Status.unique() == 'Completed'):
 
     #The summary of total Waste and Emission
     today = datetime.today()
-    current_prd = today.strftime('%Y%m')
+    current_prd = today.strftime('%y-%m')
     first = today.replace(day=1)
     last_month = first - timedelta(days=1)
-    prior_prd = last_month.strftime('%Y%m')
+    prior_prd = last_month.strftime('%y-%m')
 
     st.title('📊Emission Metrics')                                          #Page Title
     st.markdown("##")
