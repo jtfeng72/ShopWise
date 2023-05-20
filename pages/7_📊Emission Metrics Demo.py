e1df29235330aaeca99696a7af907d485b0fe2a1
csun32 <114749799+csun32@users.noreply.github.com>
2023-05-19 19:38:58 -0500

    Update 7_📊Emission Metrics Demo.py

--- pages/7_📊Emission Metrics Demo.py
+++ pages/7_📊Emission Metrics Demo.py
@@ -166,4 +166,13 @@ elif any(df.Status.unique() == 'Completed'):
     st.plotly_chart(fig_emis_by_mth, use_container_width=True)
     #right_column.plotly_chart(fig_product_sales, use_container_width=True)
 else:
-    st.warning('Sorry, not enough data available at this moment.', icon="🙇‍♂️")
+    st.warning('Sorry, not enough data available at this moment.', icon="🙇‍♂️"
+               
+ ###----Code Explain---###
+st.code('''if df.empty:
+    st.warning('Sorry, no data available at this moment.', icon="🙇‍♂️")
+elif any(df.Status.unique() == 'Completed'):
+…
+else:
+    st.warning('Sorry, not enough data available at this moment.', icon="🙇‍♂️")'''
+, language="python", line_numbers=False)
